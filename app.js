ba62abc2c5060139d3698c268d463c955d720b0d
david <dav1dch@outlook.com>
2020-08-12 13:48:59 +0800

    change

--- app.js
+++ app.js
@@ -18,55 +18,55 @@ App({
     wx.setStorageSync('logs', logs)
     var that = this
     // 登录
-    wx.login({
-      success: function(res) {
-        if (res.code) {
-          console.log("res.code:" + res.code);
-          wx.request({
-            url: that.globalData.url + '/getOpenId',
-            method: 'POST',
-            data: {
-              "code": res.code
-            },
-            success: res => {
-              console.log(res.data)
-              that.globalData.openId = res.data
-              wx.request({
-                url: that.globalData.url + '/goWithU',
-                method: 'POST',
-                header: {
-                  'Content-Type': 'application/json;charset=UTF-8'
-                },
-                data: {
-                  "openId":res.data 
-                },
-                success: res => {
-                  that.globalData.type = res.data.type
-                  that.globalData.name = res.data.name
-                }
-              })
-            },
-            fail: res => {
-              console.log(res)
-            }
-          })
-        } else {
-          console.log('获取用户登录态失败！' + res.errMsg)
-        }
-      }
+    // wx.login({
+    //   success: function(res) {
+    //     if (res.code) {
+    //       console.log("res.code:" + res.code);
+    //       wx.request({
+    //         url: that.globalData.url + '/getOpenId',
+    //         method: 'POST',
+    //         data: {
+    //           "code": res.code
+    //         },
+    //         success: res => {
+    //           console.log(res.data)
+    //           that.globalData.openId = res.data
+    //           wx.request({
+    //             url: that.globalData.url + '/goWithU',
+    //             method: 'POST',
+    //             header: {
+    //               'Content-Type': 'application/json;charset=UTF-8'
+    //             },
+    //             data: {
+    //               "openId":res.data 
+    //             },
+    //             success: res => {
+    //               console.log(res)
+    //               that.globalData.type = res.data.type
+    //               that.globalData.name = res.data.name
+    //               that.globalData.flag = true
+    //             }
+    //           })
+    //         },
+    //         fail: res => {
+    //           console.log(res)
+    //         }
+    //       })
+    //     } else {
+    //       console.log('获取用户登录态失败！' + res.errMsg)
+    //     }
+    //   }
 
-    })
+    // })
     // 获取用户信息
     wx.getSetting({
       success: res => {
 
         if (1) {
-          console.log("getUserInfo")
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              console.log(res.userInfo)
               this.globalData.userInfo = res.userInfo
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
